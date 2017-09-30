fcc44b16aee387c6ae5ed8dd5db295fa8f9acbc1
Cristy Lucke <cristy.lucke@gmail.com>
2017-09-30 23:12:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 var musicians = ["John Lennon", "Paul McCartney", "Ringo Starr", "George Harrison"]
 var instruments = ["rhythm guitar", "bass", "drums", "lead guitar"]
 
+const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+
 function theBeatlesPlay(musicians, instruments) {
   var theBeatlesPlayArray = []
 
@@ -10,8 +17,15 @@ function theBeatlesPlay(musicians, instruments) {
   return theBeatlesPlayArray
 }
 
-function johnLennonFacts() {
+function johnLennonFacts(facts) {
+  var excitedFacts = []
+  var n = 0
 
+  while (n < facts.length) {
+    excitedFacts[n] = facts[n].concat("!!!")
+    n += 1
+  }
+  return excitedFacts
 }
 
 //1) Beatles Loops returns an array of strings containing what instruments each musician plays:
